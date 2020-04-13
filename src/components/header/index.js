8a8fe9d42d29a7688ea03f49a03ca549984587a4
idatle <ledatdac@gmail.com>
2020-04-12 21:03:32 -0700

    ANimate in, not out

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,43 @@
-import { Link } from "gatsby"
-import React from "react"
-import "./header.scss"
-import ThreeDots from "../../images/three-dots.svg"
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import classnames from 'classnames'
+import './header.scss'
+import ThreeDots from '../../images/three-dots.svg'
 
 const Header = () => {
+  const [isOverlayActive, setIsOverlayActive] = useState(false)
+
+  const overlay = classnames('overlay', {
+    active: isOverlayActive,
+    // inactive: !isOverlayActive,
+  })
+
+  const openOverlay = e => {
+    setIsOverlayActive(!isOverlayActive)
+  }
+
   return (
     <div className="header">
       <div className="left-side">
-        <ThreeDots className="vertical-three-dots" />
+        <ThreeDots
+          className="vertical-three-dots"
+          onClick={e => openOverlay(e)}
+        />
+
         <h1>
           <Link to="/">NO CHANCE</Link>
         </h1>
-        <div className="links">
-          <Link to="/featured">Featured</Link>
-          <Link to="/events">Events</Link>
-          <Link to="/podcast">Podcast</Link>
-          <Link to="/about">About</Link>
+
+        <div className={overlay}>
+          <div className="links">
+            <Link to="/featured">Featured</Link>
+            <Link to="/events">Events</Link>
+            <Link to="/podcast">Podcast</Link>
+            <Link to="/about">About</Link>
+          </div>
         </div>
       </div>
+
       <div className="right-side"></div>
     </div>
   )
